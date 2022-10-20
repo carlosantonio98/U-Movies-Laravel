a68c685cc8c6afae8d905399632be1b99287db36
carloscamacho <carlosalvarez9805@gmail.com>
2022-10-19 23:14:56 -0500

    Change visitor navigation bar style

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-gray-800" x-data="{ open: false }">
+<nav x-data="{ open: false }" class="absolute top-0 left-0 z-10 w-full bg-gray-800 sm:bg-transparent">
 
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 
@@ -22,7 +22,7 @@
             </div>
 
             {{-- Logo and pc menu --}}
-            <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
                 
                 {{-- Logo --}}
                 <div class="flex flex-shrink-0 items-center">
@@ -32,6 +32,7 @@
                 
                 {{-- Pc Menu --}}
                 <div class="hidden sm:ml-6 sm:block">
+                    <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
 
                     {{-- Categories dropdown --}}
                     <div class="relative inline-block text-left" x-data="{ open: false }">
@@ -46,70 +47,23 @@
                             </button>
                         </div>
                         
-                        <div x-show="open" x-on:click.away=" open = false " class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                        <div x-show="open" x-on:click.away=" open = false " class="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                             <div class="py-1" role="none">
 
                                 @foreach ($categories as $category)
-                                    <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">{{ $category->name }}</a>
+                                    <a href="#" class="text-gray-700 hover:text-green-500 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">{{ $category->name }}</a>
                                 @endforeach
 
                             </div>
                         </div>
                     </div>
 
+                    <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">News</a>
+                    
+                    <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Premiere</a>
                 </div>
 
             </div>
-
-            {{-- Buttons auth --}}
-            @auth
-                <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-
-                    {{-- Button notifications --}}
-                    <button type="button" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                    
-                        <span class="sr-only">View notifications</span>
-                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-                        </svg>
-                    
-                    </button>
-    
-                    {{-- Profile dropdown --}}
-                    <div class="relative ml-3" x-data="{ open: false }">
-                        
-                        {{-- Profile dropdown buuton --}}
-                        <div>
-                            <button x-on:click=" open = true " type="button" class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                <span class="sr-only">Open user menu</span>
-                                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                            </button>
-                        </div>
-    
-                        {{-- Profile dropdown items --}}
-                        <div x-show="open" x-on:click.away=" open = false " class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-
-                            <a href="{{ route('profile.show') }}" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Tu perfil</a>
-                            <a href="{{ route('admin.home') }}" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Dashboard</a>
-                            <form method="POST" action="{{ route('logout') }}" x-data>
-                                @csrf
-                                <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2" @click.prevent="$root.submit();">Cerrar sesión</a>
-                            </form>
-    
-                        </div>
-
-                    </div>
-
-                </div>
-            @else
-
-                {{-- Buttons register and login --}}
-                <div>
-                    <a href="{{ route('login') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Login</a>
-                    <a href="{{ route('register') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Register</a>
-                </div>
-
-            @endif
 
         </div>
     </div>
@@ -118,11 +72,35 @@
     {{-- Mobile menu --}}
     <div class="sm:hidden" id="mobile-menu" x-show="open" x-on:click.away=" open = false ">
         <div class="space-y-1 px-2 pt-2 pb-3">
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</a>
 
-            @foreach ($categories as $category)
-                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">{{ $category->name }}</a>
-            @endforeach
-        
+            {{-- Categories dropdown --}}
+            <div class="relative w-full inline-block text-left" x-data="{ open: false }">
+                <div>
+                    <button x-on:click=" open = true " type="button" class="w-full flex text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                        
+                        Categories
+                        <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                        </svg>
+
+                    </button>
+                </div>
+                
+                <div x-show="open" x-on:click.away=" open = false " class="absolute left-0  w-full mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                    <div class="py-1" role="none">
+
+                        @foreach ($categories as $category)
+                            <a href="#" class="text-gray-700 hover:text-green-500 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">{{ $category->name }}</a>
+                        @endforeach
+
+                    </div>
+                </div>
+            </div>
+            
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">News</a>
+                    
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Premiere</a>
         </div>
     </div>
 
