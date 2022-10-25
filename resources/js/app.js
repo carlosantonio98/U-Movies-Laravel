316128070bb0cfea656ef8bc2775f79cbbb52271
carloscamacho <carlosalvarez9805@gmail.com>
2022-10-25 17:19:52 -0500

    Create scrollTop button

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,10 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// ScrollTop
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top');
+    if (this.scrollY >= 40) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('scroll-top');
+}
+window.addEventListener('scroll', scrollTop);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="scroll-smooth">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -24,6 +24,11 @@
     </head>
     <body class="font-sans antialiased bg-[#000210]">
 
+
+        <!--Scroll Top --> 
+        <a href="#" class="scrolltop" id="scroll-top">
+            <i class="fa-solid fa-chevron-up scrolltop__icon"></i>
+        </a>
 
         @livewire('navigation')
 
