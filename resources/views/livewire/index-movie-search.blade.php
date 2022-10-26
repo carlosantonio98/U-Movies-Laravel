2b2b342ac8d81eee7c096314b450430185beb6e5
Joan Arturo Camacho Alvarez <joanarturo81@gmail.com>
2022-10-25 20:55:39 -0500

    Change the separation of the content and the pager

--- resources/views/livewire/index-movie-search.blade.php
+++ resources/views/livewire/index-movie-search.blade.php
@@ -36,7 +36,7 @@
         <p class="text-sm text-gray-400">{{ $movies->count() }} / {{ $movies->total() }} Movies</p>
     </div>
     
-    <div class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-5 mb-8">
+    <div class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-5 mb-4">
         @forelse ($movies as $movie)
             <x-card-movie :$movie :isPremiere="$movie->premier"></x-card-movie>
         @empty
--- resources/views/movies/partials/grid-movies.blade.php
+++ resources/views/movies/partials/grid-movies.blade.php
@@ -1,5 +1,5 @@
 {{-- Movies --}}
-<div class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-5 mb-8">
+<div class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-5 mb-4">
     @forelse ($movies as $movie)
         <x-card-movie :$movie :isPremiere="$movie->premier == 2"></x-card-movie>
     @empty
