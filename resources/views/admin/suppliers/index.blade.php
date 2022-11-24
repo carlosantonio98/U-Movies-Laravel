fdea9148c6f1e3f9f7323ca806abf83ae1cd51c6
carloscamacho <carlosalvarez9805@gmail.com>
2022-11-23 22:05:25 -0600

    Add style to the suppliers table

--- resources/views/admin/suppliers/index.blade.php
+++ resources/views/admin/suppliers/index.blade.php
@@ -1,16 +1,30 @@
 @extends('../../layouts/app-admin')
 
-@section('title', env('APP_NAME'))
+@section('title', 'Proveedores - iUMovies')
 
 @section('content_header')
     @if (session('info'))
-        <p><b>{{ session('info') }}</b></p>
+        <div class="w-full 
+                    px-4 
+                    py-2 
+                    rounded
+                    font-bold 
+                    bg-green-600 
+                    text-white">
+
+            <p>{{ session('info') }}</p>
+        </div>
     @endif
 
-    <div class="flex flex-wrap justify-between mb-4">
-        <h1 class="py-0">Lista de proveedores</h1>
-        <a href="{{ route('admin.suppliers.create') }}" class="inline-block px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded-lg">Nuevo proveedor</a>
+    <div class="flex 
+            flex-wrap 
+            justify-between 
+            mb-4">
+
+        <h3 class="py-0 text-gray-200">Proveedores</h3>
+        <a href="{{ route('admin.suppliers.create') }}" class="inline-block px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-lg">Nuevo proveedor</a>
     </div>
+
 @endsection
 
 @section('content')
